b36829bbbcee32a03ad48a226dd527e492c268f1
stelinhaz <stela.monteiro2015@gmail.com>
2025-02-25 18:59:15 +0000

    criei sprintf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -39,6 +39,37 @@ printf("Número de pontos turísticos: ");
 scanf("%d", &pontos);
 printf("Número de pontos turísticos: %d\n", pontos);
 
+
+printf("Carta 2: \n");
+
+printf("Estado: ");
+scanf("%1s", estado);
+printf("Estado: %s\n", estado);
+
+printf("Código: ");
+scanf("%s", codigo);
+printf("Código: %s\n", codigo);
+
+printf("Nome da Cidade: ");
+scanf("%s", cidade);
+printf("Nome da Cidade: %s\n", cidade);
+
+printf("População: ");
+scanf("%d", &população);
+printf("População: %d\n", população);
+
+printf("Área: ");
+scanf("%f", &área);
+printf("Área: %.2f km²\n", área);
+
+printf("PIB: ");
+scanf("%f", &pib);
+printf("PIB: %.2f Bilhões de Reais\n", pib);
+
+printf("Número de pontos turísticos: ");
+scanf("%d", &pontos);
+printf("Número de pontos turísticos: %d\n", pontos);
+
 return 0;
 
 }
